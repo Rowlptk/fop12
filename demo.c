dd489581b3d64cbf1ff0008166302c3acb007767
Rahul Pathak <rahulzha93@gmail.com>
2020-02-24 15:08:04 +0545

    flowchart

---
+++ demo.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{
+    int num1,num2,Sum;
+    num1 = 45;
+    num2 = 56;
+
+    Sum = num1 + num2;
+
+    printf("The Sum is %d",Sum);
+}
---
+++ demo.py
@@ -0,0 +1,6 @@
+num1 = 23
+num2 = 56
+
+sum = num1 + num2
+
+print(sum)
